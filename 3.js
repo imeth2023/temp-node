bf0c0efd79c0565ccdc0d43e641fdd1018ecbe6b
imethamarajeewa <125963062+imethamarajeewa@users.noreply.github.com>
2023-10-13 22:46:25 +0530

    first commit

---
+++ 3.js
@@ -0,0 +1,13 @@
+//modules
+const names = require('./4')
+const sayHi = require('./5')
+
+
+const data = require('./6-alternate')
+
+
+require('./7')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
+

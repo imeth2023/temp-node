9c3f5d6c5898027315645b84d55df94fce8f3826
imethamarajeewa <125963062+imethamarajeewa@users.noreply.github.com>
2023-10-14 12:20:21 +0530

    ne edit

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+const http = require('http');
 
-console.log('first')
-setTimeout(() => {
-    console.log('second')
-}, 0)
-console.log('third')
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end('Welcome to our home page');
+    }
+    if (req.url === '/about') {
+        res.end('Here is our short history');
+    }
+    res.end(`error page)`)
+}
+)
+
+server.listen(3000, () => {
+    console.log('Server listening on port : 5000....');
+}   )
